f8591af3650f317d225124efd9d9c67cfa645558
Samuel Farfeleder <s.farfeleder@students.htl.leonding.ac.at>
2025-04-11 10:50:07 +0200

    json zu js gemacht und dieses mit allen Spieler fertig gemacht

---
+++ project/JSON/Spieler.js
@@ -0,0 +1,280 @@
+
+ const kader={
+
+"spieler":[
+    {
+        "name": "Marc-André ter Stegen",
+        "alter": 32,
+        "spiele": 420,
+        "nr": 1,
+        "position": "Torwart",
+        "natio": "Deutschland",
+        "wert": "15 MIO",
+        "zunull": 174,
+        "gehalten": 983
+    },
+    {
+     "name": "Iñaki Peña" ,
+        "alter": 26,
+        "spiele": 44 ,
+        "nr":  13,
+        "position": "Torwart",
+        "natio": "Spanien",
+        "wert": "10 MIO",
+        "zunull": 9 ,
+        "gehalten": 88
+    },
+    {
+        "name": "Wojciech Szczęsny",
+           "alter": 34,
+           "spiele": 19,
+           "nr": 25,
+           "position":"Torwart" ,
+           "natio": "Polen" ,
+           "wert": "1 MIO",
+           "zunull": 10,
+           "gehalten": 37
+    },
+    {
+    "name": "Pau Cubarsi",
+    "alter": 18,
+    "spiele": 19,
+    "nr": 2,
+    "position":"Verteidiger" ,
+    "natio": "Spanien" ,
+    "wert": "70 MIO",
+    "tore": 1,
+    "vorlagen": 4
+},
+{
+    "name": "Alejandro Balde",
+    "alter": 21,
+    "spiele": 119,
+    "nr": 3,
+    "position":"Verteidiger" ,
+    "natio": "Spanien" ,
+    "wert": "60 MIO",
+    "tore": 3,
+    "vorlagen": 15
+},
+{
+    "name": "Ronald Araujo",
+    "alter": 26,
+    "spiele": 164,
+    "nr": 4,
+    "position":"Verteidiger" ,
+    "natio": "Spanien" ,
+    "wert": "50 MIO",
+    "tore": 10,
+    "vorlagen": 7
+},
+{
+    "name": "Iñigo Martínez",
+    "alter": 33,
+    "spiele": 62,
+    "nr": 5,
+    "position":"Verteidiger" ,
+    "natio": "Spanien" ,
+    "wert": "5 MIO",
+    "tore": 3,
+    "vorlagen": 4
+},
+{
+    "name": "Andreas Christensen",
+    "alter": 28,
+    "spiele": 75,
+    "nr": 15,
+    "position":"Verteidiger" ,
+    "natio": "Dänemark" ,
+    "wert": "5 MIO",
+    "tore": 4,
+    "vorlagen": 3
+},
+{
+    "name": "Jules Kounde",
+    "alter": 26,
+    "spiele": 134,
+    "nr": 23,
+    "position":"Verteidiger" ,
+    "natio": "Frankreich" ,
+    "wert": "60 MIO",
+    "tore": 6,
+    "vorlagen": 18
+},
+{
+    "name": "Eric Garcia",
+    "alter": 24,
+    "spiele": 100,
+    "nr": 24,
+    "position":"Verteidiger" ,
+    "natio": "Spanien" ,
+    "wert": "60 MIO",
+    "tore": 6,
+    "vorlagen": 18
+},
+{
+    "name": "Gerard Martin",
+    "alter": 23,
+    "spiele": 30,
+    "nr": 35,
+    "position":"Verteidiger" ,
+    "natio": "Spanien" ,
+    "wert": "7.5 MIO",
+    "tore": 1,
+    "vorlagen": 3
+},
+{
+    "name": "Hector Fort",
+    "alter": 18,
+    "spiele": 15,
+    "nr": 24,
+    "position":"Verteidiger" ,
+    "natio": "Spanien" ,
+    "wert": "10 MIO",
+    "tore": 0,
+    "vorlagen": 0
+},
+{
+    "name": "Gavi",
+    "alter": 20,
+    "spiele": 141,
+    "nr": 6,
+    "position":"Midfielder" ,
+    "natio": "Spanien" ,
+    "wert": "70 MIO",
+    "tore": 10,
+    "vorlagen": 14
+},
+{
+    "name": "Marc Casado",
+    "alter": 21,
+    "spiele": 41,
+    "nr": 17,
+    "position":"Midfielder" ,
+    "natio": "Spanien" ,
+    "wert": "30 MIO",
+    "tore": 1,
+    "vorlagen": 6
+},
+{
+    "name": "Marc Bernal",
+    "alter": 17,
+    "spiele": 3,
+    "nr": 28,
+    "position":"Midfielder" ,
+    "natio": "Spanien" ,
+    "wert": "5 MIO",
+    "tore": 0,
+    "vorlagen": 0
+},
+{
+    "name": "Pedri",
+    "alter": 22,
+    "spiele": 190,
+    "nr": 8,
+    "position":"Midfielder" ,
+    "natio": "Spanien" ,
+    "wert": "120 MIO",
+    "tore": 25,
+    "vorlagen": 20
+},
+{
+    "name": "Pablo Torre",
+    "alter": 22,
+    "spiele": 27,
+    "nr": 14,
+    "position":"Midfielder" ,
+    "natio": "Spanien" ,
+    "wert": "7.5 MIO",
+    "tore": 5,
+    "vorlagen": 4
+},
+{
+    "name": "Fermin Lopez",
+    "alter": 21,
+    "spiele": 76,
+    "nr": 16,
+    "position":"Midfielder" ,
+    "natio": "Spanien" ,
+    "wert": "50 MIO",
+    "tore": 16,
+    "vorlagen": 8
+},
+{
+    "name": "Dani Olmo",
+    "alter": 26,
+    "spiele": 28,
+    "nr": 20,
+    "position":"Midfielder" ,
+    "natio": "Spanien" ,
+    "wert": "60 MIO",
+    "tore": 8,
+    "vorlagen": 5
+},
+{
+    "name": "Frenkie de Jong",
+    "alter": 27,
+    "spiele": 247,
+    "nr": 21,
+    "position":"Midfielder" ,
+    "natio": "Niederland" ,
+    "wert": "45 MIO",
+    "tore": 19,
+    "vorlagen": 21
+},
+{
+    "name": "Ferran Torres",
+    "alter": 25,
+    "spiele": 150,
+    "nr": 7,
+    "position":"Forward" ,
+    "natio": "Spanien" ,
+    "wert": "28 MIO",
+    "tore": 41,
+    "vorlagen": 16
+},
+{
+    "name": "Robert Lewandowski",
+    "alter": 36,
+    "spiele": 140,
+    "nr": 9,
+    "position":"Forward" ,
+    "natio": "Polen" ,
+    "wert": "15 MIO",
+    "tore": 99,
+    "vorlagen": 20
+},
+{
+    "name": "Raphinha",
+    "alter": 28,
+    "spiele": 132,
+    "nr": 11,
+    "position":"Forward" ,
+    "natio": "Brasilien" ,
+    "wert": "80 MIO",
+    "tore": 48,
+    "vorlagen": 43
+},
+{
+    "name": "Pau Victor",
+    "alter": 23,
+    "spiele": 22,
+    "nr": 18,
+    "position":"Forward" ,
+    "natio": "Spanien" ,
+    "wert": "5 MIO",
+    "tore": 2,
+    "vorlagen": 1
+},
+{
+    "name": "Lamine Yamal",
+    "alter": 17,
+    "spiele": 93,
+    "nr": 19,
+    "position":"Forward" ,
+    "natio": "Spanien" ,
+    "wert": "180 MIO",
+    "tore": 21,
+    "vorlagen": 24
+}    
+]}
