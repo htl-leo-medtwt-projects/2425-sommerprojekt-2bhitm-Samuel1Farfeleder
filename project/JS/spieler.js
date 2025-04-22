fb0dd8dc64771bf02fdc7edfa36b8c7249abe86a
Samuel Farfeleder <s.farfeleder@students.htl.leonding.ac.at>
2025-04-22 14:00:44 +0200

    spieler seite

---
+++ project/JS/spieler.js
@@ -0,0 +1,411 @@
+const kader = {
+    "spieler": [
+      {
+        "name": "Marc-André ter Stegen",
+        "alter": 32,
+        "spiele": 420,
+        "nr": 1,
+        "position": "Torwart",
+        "natio": "Deutschland",
+        "wert": "15 MIO",
+        "zunull": 174,
+        "gehalten": 983,
+        "bild":"https://www.fcbarcelona.com/photo-resources/2024/10/13/04c3b761-ccbd-498f-b988-b67ed1693906/01-Ter_Stegen-M.png?width=300&height=354"
+      },
+      {
+        "name": "Iñaki Peña",
+        "alter": 26,
+        "spiele": 44,
+        "nr": 13,
+        "position": "Torwart",
+        "natio": "Spanien",
+        "wert": "10 MIO",
+        "zunull": 9,
+        "gehalten": 88,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/c058dc89-fa0b-4aa9-99de-a129ece52320/13-Inaki_Pena-M.png?width=670&height=790"
+      },
+      {
+        "name": "Wojciech Szczęsny",
+        "alter": 34,
+        "spiele": 19,
+        "nr": 25,
+        "position": "Torwart",
+        "natio": "Polen",
+        "wert": "1 MIO",
+        "zunull": 10,
+        "gehalten": 37,
+        "bild":"https://www.fcbarcelona.com/photo-resources/2024/10/14/a62932d1-106c-4762-84bf-89fa9a633c71/25-Szczesny-M.png?width=670&height=790"
+      },
+      {
+        "name": "Pau Cubarsi",
+        "alter": 18,
+        "spiele": 19,
+        "nr": 2,
+        "position": "Verteidiger",
+        "natio": "Spanien",
+        "wert": "70 MIO",
+        "tore": 1,
+        "vorlagen": 4,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/951e72f0-59ae-4685-be81-f2afced7e6d6/02-Cubarsi-M.png?width=670&height=790"
+      },
+      {
+        "name": "Alejandro Balde",
+        "alter": 21,
+        "spiele": 119,
+        "nr": 3,
+        "position": "Verteidiger",
+        "natio": "Spanien",
+        "wert": "60 MIO",
+        "tore": 3,
+        "vorlagen": 15,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/e3784cf8-9f03-45e2-ab44-007f2adadf32/03-Balde-M.png?width=300&height=354"
+      },
+      {
+        "name": "Ronald Araujo",
+        "alter": 26,
+        "spiele": 164,
+        "nr": 4,
+        "position": "Verteidiger",
+        "natio": "Spanien",
+        "wert": "50 MIO",
+        "tore": 10,
+        "vorlagen": 7,
+        "bild" : "https://www.fcbarcelona.com/photo-resources/2024/10/13/5352ecab-dc7a-4d20-a047-67af419dd96b/04-Araujo-M.png?width=300&height=354"
+      },
+      {
+        "name": "Iñigo Martínez",
+        "alter": 33,
+        "spiele": 62,
+        "nr": 5,
+        "position": "Verteidiger",
+        "natio": "Spanien",
+        "wert": "5 MIO",
+        "tore": 3,
+        "vorlagen": 4,
+        "bild" : "https://www.fcbarcelona.com/photo-resources/2024/10/13/3129af5f-0ec1-4474-988c-1415092eb74f/05-Martinez-M.png?width=300&height=354"
+      },
+      {
+        "name": "Andreas Christensen",
+        "alter": 28,
+        "spiele": 75,
+        "nr": 15,
+        "position": "Verteidiger",
+        "natio": "Dänemark",
+        "wert": "5 MIO",
+        "tore": 4,
+        "vorlagen": 3,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/0969d283-c3f8-471a-8638-9a37124ea1a0/15-Christensen-M.png?width=300&height=354"
+      },
+      {
+        "name": "Jules Kounde",
+        "alter": 26,
+        "spiele": 134,
+        "nr": 23,
+        "position": "Verteidiger",
+        "natio": "Frankreich",
+        "wert": "60 MIO",
+        "tore": 6,
+        "vorlagen": 18,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/9f7d8d15-0244-4def-a931-8eb563a598a1/23-Kounde-M.png?width=300&height=354"
+      },
+      {
+        "name": "Eric Garcia",
+        "alter": 24,
+        "spiele": 100,
+        "nr": 24,
+        "position": "Verteidiger",
+        "natio": "Spanien",
+        "wert": "60 MIO",
+        "tore": 6,
+        "vorlagen": 18,
+        "bild" : "https://www.fcbarcelona.com/photo-resources/2024/10/13/60ec100e-ca62-492c-a3c7-7e73956b035b/24-Eric_Garcia-M.png?width=300&height=354"
+      },
+      {
+        "name": "Gerard Martin",
+        "alter": 23,
+        "spiele": 30,
+        "nr": 35,
+        "position": "Verteidiger",
+        "natio": "Spanien",
+        "wert": "7.5 MIO",
+        "tore": 1,
+        "vorlagen": 3,
+        "bild": "https://i.namu.wiki/i/VwsAxlGK91df0bmj7rC1nugcsFc1E58PQvpV_eGXVjynFFkSA3_YpymmkzGTpdA-DSGn3LSKcltSstgNDgEvEA.webp"
+      },
+      {
+        "name": "Hector Fort",
+        "alter": 18,
+        "spiele": 15,
+        "nr": 24,
+        "position": "Verteidiger",
+        "natio": "Spanien",
+        "wert": "10 MIO",
+        "tore": 0,
+        "vorlagen": 0,
+        "bild": "https://img.uefa.com/imgml/TP/players/1/2025/cutoff/250171667.webp"
+      },
+      {
+        "name": "Gavi",
+        "alter": 20,
+        "spiele": 141,
+        "nr": 6,
+        "position": "Midfielder",
+        "natio": "Spanien",
+        "wert": "70 MIO",
+        "tore": 10,
+        "vorlagen": 14,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/545795b7-4c63-4347-94ab-5f2a49b1eddd/06-Gavi-M.png?width=300&height=354"
+
+      },
+      {
+        "name": "Marc Casado",
+        "alter": 21,
+        "spiele": 41,
+        "nr": 17,
+        "position": "Midfielder",
+        "natio": "Spanien",
+        "wert": "30 MIO",
+        "tore": 1,
+        "vorlagen": 6,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/d9fb4b17-1260-48a5-841d-ffd2a60e57e4/17-Casado-M.png?width=300&height=354"
+      },
+      {
+        "name": "Marc Bernal",
+        "alter": 17,
+        "spiele": 3,
+        "nr": 28,
+        "position": "Midfielder",
+        "natio": "Spanien",
+        "wert": "5 MIO",
+        "tore": 0,
+        "vorlagen": 0,
+        "bild": "https://assets.laliga.com/squad/2024/t178/p637126/2048x2225/p637126_t178_2024_1_001_000.png"
+      },
+      {
+        "name": "Pedri",
+        "alter": 22,
+        "spiele": 190,
+        "nr": 8,
+        "position": "Midfielder",
+        "natio": "Spanien",
+        "wert": "120 MIO",
+        "tore": 25,
+        "vorlagen": 20,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/61d91e06-7435-4823-93e3-2ef7e91b3c8b/08-Pedri-M.png?width=300&height=354"
+      },
+      {
+        "name": "Pablo Torre",
+        "alter": 22,
+        "spiele": 27,
+        "nr": 14,
+        "position": "Midfielder",
+        "natio": "Spanien",
+        "wert": "7.5 MIO",
+        "tore": 5,
+        "vorlagen": 4,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/0b7225dc-8ec1-4957-bb92-3ebd72f00dd7/14-Pablo_Torre-M.png?width=300&height=354"
+      },
+      {
+        "name": "Fermin Lopez",
+        "alter": 21,
+        "spiele": 76,
+        "nr": 16,
+        "position": "Midfielder",
+        "natio": "Spanien",
+        "wert": "50 MIO",
+        "tore": 16,
+        "vorlagen": 8,
+        "bild" : "https://www.fcbarcelona.com/photo-resources/2024/10/13/bc22e993-3083-4ef5-aa77-217951756cca/16-Fermin-M.png?width=300&height=354"
+      },
+      {
+        "name": "Dani Olmo",
+        "alter": 26,
+        "spiele": 28,
+        "nr": 20,
+        "position": "Midfielder",
+        "natio": "Spanien",
+        "wert": "60 MIO",
+        "tore": 8,
+        "vorlagen": 5,
+        "bild" : "https://www.fcbarcelona.com/photo-resources/2024/10/13/6df53816-c907-4ecc-bd0a-c2e8e5a281c2/20-Olmo-M.png?width=300&height=354"
+      },
+      {
+        "name": "Frenkie de Jong",
+        "alter": 27,
+        "spiele": 247,
+        "nr": 21,
+        "position": "Midfielder",
+        "natio": "Niederland",
+        "wert": "45 MIO",
+        "tore": 19,
+        "vorlagen": 21,
+        "bild" : "https://www.fcbarcelona.com/photo-resources/2024/10/13/1f6c10a5-07cd-47b3-929b-e29ccabe18d7/21-De_Jong-M.png?width=300&height=354"
+      },
+      {
+        "name": "Ferran Torres",
+        "alter": 25,
+        "spiele": 150,
+        "nr": 7,
+        "position": "Forward",
+        "natio": "Spanien",
+        "wert": "28 MIO",
+        "tore": 41,
+        "vorlagen": 16,
+        "bild" : "https://www.fcbarcelona.com/photo-resources/2024/10/13/1edefda1-8229-4232-ace9-2f35bf4d3000/07-Ferran_Torres-M.png?width=300&height=354"
+      },
+      {
+        "name": "Robert Lewandowski",
+        "alter": 36,
+        "spiele": 140,
+        "nr": 9,
+        "position": "Forward",
+        "natio": "Polen",
+        "wert": "15 MIO",
+        "tore": 99,
+        "vorlagen": 20,
+        "bild" : "https://www.fcbarcelona.com/photo-resources/2024/10/13/894e4e5e-b6d0-434e-8610-890e0971a7b2/09-Lewandowski-M.png?width=300&height=354"
+      },
+      {
+        "name": "Raphinha",
+        "alter": 28,
+        "spiele": 132,
+        "nr": 11,
+        "position": "Forward",
+        "natio": "Brasilien",
+        "wert": "80 MIO",
+        "tore": 48,
+        "vorlagen": 43,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/266dc5af-16b0-4290-99c5-03d73a48df11/11-Raphinha-M.png?width=300&height=354"
+      },
+      {
+        "name": "Pau Victor",
+        "alter": 23,
+        "spiele": 22,
+        "nr": 18,
+        "position": "Forward",
+        "natio": "Spanien",
+        "wert": "5 MIO",
+        "tore": 2,
+        "vorlagen": 1,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/64d7a058-2043-4a39-a5b6-2b7525541fd5/18-Pau_Victor-M.png?width=300&height=354"
+      },
+      {
+        "name": "Ansu Fati",
+        "alter": 22,
+        "spiele": 121,
+        "nr": 10,
+        "position": "Forward",
+        "natio": "Spanien",
+        "wert": "5 MIO",
+        "tore": 29,
+        "vorlagen": 6,
+        "bild": "https://www.fcbarcelona.com/photo-resources/2024/10/13/64d7a058-2043-4a39-a5b6-2b7525541fd5/18-Pau_Victor-M.png?width=300&height=354"
+      },
+      {
+        "name": "Lamine Yamal",
+        "alter": 17,
+        "spiele": 93,
+        "nr": 19,
+        "position": "Forward",
+        "natio": "Spanien",
+        "wert": "180 MIO",
+        "tore": 21,
+        "vorlagen": 24,
+        "bild" : "https://www.fcbarcelona.com/photo-resources/2024/10/13/d942d5c0-5ada-423c-baf1-4d2ba5ebe3bb/19-Lamine-M.png?width=300&height=354"
+      }
+    ]
+  };
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.getElementById("player-grid");
+    const searchInput = document.getElementById("search");
+    const filterButtons = document.querySelectorAll("#filters button");
+    const modal = document.getElementById("player-modal");
+    const profileDetails = document.getElementById("profile-details");
+    const closeModal = document.querySelector(".close");
+  
+    const spieler = kader.spieler;
+  
+    function createCard(player) {
+      const card = document.createElement("div");
+      card.classList.add("player-card");
+  
+      card.innerHTML = `
+        <img src="${player.bild}" alt="${player.name}">
+        <p class="highlight">${player.name}</p>
+        <div class="hover-info">
+          <p>Position: ${player.position}</p>
+          <p>Nummer: ${player.nr}</p>
+          <p>Nationalität: ${player.natio}</p>
+        </div>
+        <div class="player-info-box">${player.wert}</div>
+      `;
+  
+      card.addEventListener("click", () => openModal(player));
+      return card;
+    }
+  
+    function openModal(player) {
+      modal.classList.add("active");
+  
+      let extraStats = "";
+  
+      if (player.position === "Torwart") {
+        extraStats = `
+          <p>Spiele: ${player.spiele}</p>
+          <p>Zu Null: ${player.zunull}</p>
+          <p>Gehaltene Bälle: ${player.gehalten}</p>
+        `;
+      } else {
+        extraStats = `
+          <p>Spiele: ${player.spiele}</p>
+          <p>Tore: ${player.tore}</p>
+          <p>Vorlagen: ${player.vorlagen}</p>
+        `;
+      }
+  
+      profileDetails.innerHTML = `
+        <img src="${player.bild}" alt="${player.name}">
+        <h2>${player.name}</h2>
+        <p>Alter: ${player.alter}</p>
+        <p>Position: ${player.position}</p>
+        <p>Wert: <span class="highlight">${player.wert}</span></p>
+        ${extraStats}
+
+      `;
+    }
+  
+    closeModal.addEventListener("click", () => {
+      modal.classList.remove("active");
+    });
+  
+    function renderPlayers(filteredPlayers) {
+      grid.innerHTML = "";
+      filteredPlayers.forEach(player => {
+        grid.appendChild(createCard(player));
+      });
+    }
+  
+    renderPlayers(spieler);
+  
+    filterButtons.forEach(button => {
+      button.addEventListener("click", () => {
+        const pos = button.getAttribute("data-pos");
+        if (pos === "All") {
+          renderPlayers(spieler);
+        } else {
+          const filtered = spieler.filter(s => s.position === pos);
+          renderPlayers(filtered);
+        }
+      });
+    });
+  
+    searchInput.addEventListener("input", (e) => {
+      const searchValue = e.target.value.toLowerCase();
+      const filtered = spieler.filter(s =>
+        s.name.toLowerCase().includes(searchValue)
+      );
+      renderPlayers(filtered);
+    });
+  });
+  
